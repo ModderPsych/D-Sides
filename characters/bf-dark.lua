8413ce8e992dc6d79dec3ef53af775df1c52bb1d
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-04-21 16:25:25 -0300

    Update and rename bf-dark.hx to bf-dark.lua

---
+++ characters/bf-dark.lua
@@ -0,0 +1,40 @@
+local char = 'boyfriend'
+
+function onCreatePost()
+    if getPropertyFromClass('backend.ClientPrefs', 'data.lowQuality') then
+        return
+    end
+
+    setProperty(char .. '.color', getColor(songName))
+
+    initLuaShader('DropShadowShader')
+    setSpriteShader(char, 'DropShadowShader')
+
+    setShaderFloat(char, 'distance', getDistance(songName))
+    setShaderFloat(char, 'angle', getAngle(songName))
+
+    setShaderFloatArray(char, 'color', {0.56, 0.44, 0.34, 0.56})
+
+    setShaderFloatArray(char, 'uFrameBounds', {-0.2, -1.5, 2, 2})
+end
+
+function getColor(thing)
+    if thing == 'blammed' then
+        return 0xffb5ade9
+    end
+    return 0xff7266c1
+end
+
+function getDistance(thing)
+    if thing == 'blammed' then
+        return 12
+    end
+    return 15
+end
+
+function getAngle(thing)
+    if thing == 'blammed' then
+        return 200
+    end
+    return 135
+end
